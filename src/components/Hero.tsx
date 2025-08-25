255a1cc596241d53dd1c51788d9b9ff12a081f59
nowfeel <nowfeel.safi@gmail.com>
2025-08-25 11:40:58 +0200

    feat: add web3 passion

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,31 +1,32 @@
 import { FaEnvelope } from "react-icons/fa"
 
 function Hero() {
-    return (
-        <>
-            <section className="max-w-4xl mx-auto">
-                <article className="flex flex-col items-start">
-                    <h2 className="text-xl font-medium">Hello,</h2>
-                    <h1 className="text-2xl font-bold">My name is Nowfeel </h1>
-                    <br />
-                    <br />
-                    <p className="text-lg">
-                        I'm a Software Developer focused on <b>back-end development </b>with 3 years of experience. <br />What I enjoy the most about software development
-                        is <b>solving real-world problems </b>and meeting evolving needs. <br />Currently in a contract position at Bliink, a french start-up in the digital advertising field.
-                        <br />
-                        I'm Based in Lille, France.
-                    </p>
-                </article>
-                <div className="mt-4">
-                    <a href="mailto:nowfeel.dev.pro@gmail.com" target='_blank' rel='noreferrer'
-                        className="inline-flex items-center gap-3 px-4 py-3 bg-black text-white font-medium rounded-lg hover:bg-gray-500 transition-colors duration-200">
-                        <FaEnvelope />
-                        Contact me
-                    </a>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className="max-w-4xl mx-auto">
+        <article className="flex flex-col items-start">
+          <h2 className="text-xl font-medium">Hello,</h2>
+          <h1 className="text-2xl font-bold">My name is Nowfeel </h1>
+          <br />
+          <br />
+          <p className="text-lg">
+            I'm a Software Developer focused on <b>back-end development </b>with 3 years of experience. <br />What I enjoy the most about software development
+            is <b>solving real-world problems </b>and meeting evolving needs. <br />Currently in a contract position at Bliink, a french start-up in the digital advertising field.
+            <br />
+            <b>Crypto enthousiast</b>, I'm focused on learning Web3 technologies and exploring decentralized web ecosystem. <br />
+            Based in Lille, France.
+          </p>
+        </article>
+        <div className="mt-4">
+          <a href="mailto:nowfeel.dev.pro@gmail.com" target='_blank' rel='noreferrer'
+            className="inline-flex items-center gap-3 px-4 py-3 bg-black text-white font-medium rounded-lg hover:bg-gray-500 transition-colors duration-200">
+            <FaEnvelope />
+            Contact me
+          </a>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default Hero
