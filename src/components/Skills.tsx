a37ba70597532577ec3c34d09932e8f381bef409
nowfeel <nowfeel.safi@gmail.com>
2025-08-25 11:35:29 +0200

    feat: add solidity skill

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,32 +1,33 @@
 const skills = [
-    "Software Design",
-    "Golang",
-    "Relational Databases",
-    "Python",
-    "React",
-    "Docker",
+  "Software Design",
+  "Golang",
+  "Python",
+  "React",
+  "Solitidy",
+  "Relational Databases",
+  "Docker",
 ]
 
 function Skills() {
-    return <>
-        <section className="max-w-4xl mx-auto">
-            <div>
-                <h2 className="border-t-2 pt-2 text-4xl font-normal mb-12">Skills</h2>
-            </div>
-            <div>
-                {skills.map((skill, index) => (
-                    <div key={index}>
-                        <h3 className="text-5xl font-medium">
-                            {skill}
-                        </h3>
-                        {index < skills.length - 1 && (
-                            <div className="border-t border-gray-100 m-8"></div>
-                        )}
-                    </div>
-                ))}
-            </div>
-        </section>
-    </>
+  return <>
+    <section className="max-w-4xl mx-auto">
+      <div>
+        <h2 className="border-t-2 pt-2 text-4xl font-normal mb-12">Skills</h2>
+      </div>
+      <div>
+        {skills.map((skill, index) => (
+          <div key={index}>
+            <h3 className="text-5xl font-medium">
+              {skill}
+            </h3>
+            {index < skills.length - 1 && (
+              <div className="border-t border-gray-100 m-8"></div>
+            )}
+          </div>
+        ))}
+      </div>
+    </section>
+  </>
 }
 
 export default Skills;
