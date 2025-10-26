41a54ad269584efecebcc79ff16201bc0bf0db14
noz <nowfeel.safi@gmail.com>
2025-10-26 20:53:15 +0100

    refactor: remove useless files chore: Dockerfile to build and serve portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss(),
-    ],
-    assetsInclude: ["**/*.md"], // allow importing of markdown files
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  assetsInclude: ["**/*.md"], // allow importing of markdown files
 })
